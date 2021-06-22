78eb6ff36b0c06828d40913f7f2f1de42730f019
BrianRMoo <69164107+BrianRMoo@users.noreply.github.com>
2021-06-22 09:50:34 -0500

    Add files via upload
    Added load percent work in progress

--- System Information Cpp Console.cpp
+++ System Information Cpp Console.cpp
@@ -6,7 +6,13 @@
 #include <intrin.h>
 #include <iomanip>
 #include <string.h>
+#include <Windows.h>
 using namespace std;
+
+
+static float CalculateLoadPercent();
+static unsigned long long FileLoadTickCheck();
+float GetCPULoad();
 
 void DisplayCpuInformation()
 {
@@ -35,7 +41,7 @@ void DisplayCpuInformation()
 	cout << "\n\n---------------------------------------------------------------\n";	
 	cout << setw(1) << " " << setw(36)  << "CPU Information";
 	cout << "\n---------------------------------------------------------------\n";
-	printf("CPU: ");
+	printf("	CPU: ");
 	for (i = 0x80000000; i <= nExIds; ++i) {
 		__cpuid(CPUInfo, i);
 		if (i == 0x80000002) {
@@ -52,7 +58,7 @@ void DisplayCpuInformation()
 	printf("%s", CpuBrandString);
 	printf("\n");
 	//Architecture
-	printf("Architecture:");
+	printf("	Architecture: ");
 	if (sysInfo.wProcessorArchitecture == 0) {
 		printf("x86");
 	}
@@ -74,31 +80,63 @@ void DisplayCpuInformation()
 	//printf("CPU Status:");
 	//printf("\n\n");
 	//Clock Speed
-	printf("Clock Speed: ");
+	printf("	Clock Speed: ");
 	printf("%0.1f GHz", CPUInfo[1] * .01);
 	printf("\n");
 	//Load Percent
-	printf("Load Percent");
+	printf("	Load Percent");
 	printf("\n");
 	
 	//Clock Frequency
-	printf("Clock Frequency: ");
-	unsigned __int64 nStartTime, nEndTime, nTimeDifference, nFrequency, nStart, nEnd, nDiff;
-	if (QueryPerformanceFrequency((LARGE_INTEGER*)&nFrequency)) {
-		QueryPerformanceCounter((LARGE_INTEGER*)&nStartTime);
-		nStart = __rdtsc();
+	printf("	Clock Frequency: ");
+	unsigned __int64 FreCheckStartTime, FreCheckEndTime, FreCheckTimeDifference, frequency, FreCheckStart, FreCheckEnd, FreCheckDiff;
+	if (QueryPerformanceFrequency((LARGE_INTEGER*)&frequency)) {
+		QueryPerformanceCounter((LARGE_INTEGER*)&FreCheckStartTime);
+		FreCheckStart = __rdtsc();
 		Sleep(100);
-		QueryPerformanceCounter((LARGE_INTEGER*)&nEndTime);
-		nEnd = __rdtsc();
-		nDiff = nEnd - nStart;
-		nTimeDifference = nEndTime - nStartTime;
+		QueryPerformanceCounter((LARGE_INTEGER*)&FreCheckEndTime);
+		FreCheckEnd = __rdtsc();
+		FreCheckDiff = FreCheckEnd - FreCheckStart;
+		FreCheckTimeDifference = FreCheckEndTime - FreCheckStartTime;
 
-		__int64 n_Frequency = (unsigned __int64)(((double)nDiff) / (((double)nTimeDifference) / nFrequency));
+		__int64 n_Frequency = (unsigned __int64)(((double)FreCheckDiff) / (((double)FreCheckTimeDifference) / frequency));
 		(double)n_Frequency;
+		//Get freq in x.x% format
 		printf("%0.1f GHz", n_Frequency * .000000001);
+		cout << PrintCPULoad();
+		
 	}
 	
 	printf("\n\n");
+}
+int PrintCPULoad() {
+	int cpuLoad = 0;
+	while (cpuLoad == 0) {
+		std::cout << GetCPULoad() * 100;
+		Sleep(1000);
+	}
+	return 0;
+}
+static float CalculateLoadPercent(unsigned long long idle, unsigned long long ticks) {
+	static unsigned long long previousTicks = 0;
+	static unsigned long long previousIdleTicks = 0;
+
+	unsigned long long totalTicks = ticks - previousTicks;
+	unsigned long long idleTicks = idle - previousIdleTicks;
+
+	float loadPercent = 1.0f - ((totalTicks > 0) ? ((float)previousIdleTicks) / totalTicks : 0);
+	previousTicks = ticks;
+	previousIdleTicks = idle;
+
+	return loadPercent;
+}
+
+static unsigned long long FileLoadTickCheck(const FILETIME& filetime) {
+	return (((unsigned long long)(filetime.dwHighDateTime)) << 32) | ((unsigned long long)filetime.dwLowDateTime);
+}
+float GetCPULoad() {
+	FILETIME idleT, kernelT, userT;
+	return GetSystemTimes(&idleT, &kernelT, &userT) ? CalculateLoadPercent(FileLoadTickCheck(idleT), FileLoadTickCheck(kernelT) + FileLoadTickCheck(userT)) : -1.0f;
 }
 
 void GetGpuInformation() {
@@ -116,7 +154,7 @@ int main()
 			
 			
 	
-	("system,.pause");
+
 	
 
 
